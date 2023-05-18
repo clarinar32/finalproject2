9c0adae9c8c9b2fff31783920cd43a46414ed508
clencyc <clency2023@gmail.com>
2023-05-18 07:43:17 +0300

    initial project

---
+++ app/src/main/java/com/example/myapp/mainpage2.kt
@@ -0,0 +1,18 @@
+package com.example.myapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View.OnClickListener
+import androidx.cardview.widget.CardView
+
+class mainpage2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mainpage2)
+
+
+    }
+    fun onClicklistener(){
+
+    }
+}
